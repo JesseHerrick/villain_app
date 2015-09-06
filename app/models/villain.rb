c28c25725d341d0ccd0f2b323366c416dad3bf09
Jesse Herrick <me@jesse.codes>
2015-09-05 23:55:11 -0400

    Add Villain#last_sighted method and tests

--- app/models/villain.rb
+++ app/models/villain.rb
@@ -1,4 +1,9 @@
 class Villain < ActiveRecord::Base
   has_many :henchmen
   has_many :sightings
+
+  # Public: Returns the DateTime of the Villain's last sighting.
+  def last_sighted
+    sightings.order(:updated_at).last.updated_at
+  end
 end
--- test/models/villain_test.rb
+++ test/models/villain_test.rb
@@ -1,4 +1,14 @@
 require 'test_helper'
 
 class VillainTest < ActiveSupport::TestCase
+  describe '#last_sighted' do
+    before do
+      @villain = Villain.find ActiveRecord::FixtureSet.identify(:joker)
+    end
+
+    it 'it should return last sighting' do
+      expected = @villain.sightings.order(:updated_at).last.updated_at
+      assert_equal @villain.last_sighted, expected
+    end
+  end
 end
